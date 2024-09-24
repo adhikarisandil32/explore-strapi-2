5438fc12a0c779708609ec4ae30e3bd005dec8e2
unknown <adhikari.sandil@webexpertsnepal.com>
2024-09-24 18:12:56 +0545

    cloudinary-setup

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      providerOptions: {
+        cloud_name: env("cloudinary_cloud_name"),
+        api_key: env("cloudinary_api_key"),
+        api_secret: env("cloudinary_api_secret"),
+      },
+    },
+  },
+});
